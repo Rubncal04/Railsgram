2991a9fa8eb9f338606dc613ddf8cad35938108d
Ruben Gomez <rubncal.9501@gmail.com>
2022-03-15 14:36:32 -0500

    create method and my first image created

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,16 @@
 class ImagesController < ApplicationController
+
   def new
+  end
+
+  def create
+    # render plain: params[:image].inspect
+    @picture = Image.new image_params
+    @picture.save
+  end
+
+  private
+  def image_params
+    params.require(:image).permit :description
   end
 end
